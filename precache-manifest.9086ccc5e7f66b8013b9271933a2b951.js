963a786877b069e77471fb5ac519502951a82dc8
Hendri Faisal Hidayat <hendrifaisal18@gmail.com>
2020-05-07 20:24:49 +0700

    Updates

---
+++ precache-manifest.9086ccc5e7f66b8013b9271933a2b951.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85c1fc97c2e4646b952726ac22073f8c",
+    "url": "/pokedex-gh-pages/index.html"
+  },
+  {
+    "revision": "fcd884ba5c79e2d1ae44",
+    "url": "/pokedex-gh-pages/static/js/2.74efdc15.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/pokedex-gh-pages/static/js/2.74efdc15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06a607c748501c0cd4c9",
+    "url": "/pokedex-gh-pages/static/js/main.542bda04.chunk.js"
+  },
+  {
+    "revision": "b1db078ee37e0303cfd6",
+    "url": "/pokedex-gh-pages/static/js/runtime-main.db648dc1.js"
+  }
+]);
