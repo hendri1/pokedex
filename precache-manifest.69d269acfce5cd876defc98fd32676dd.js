24979b71970f4bc92ea53172dae9d8985f8bd227
Hendri Faisal Hidayat <hendrifaisal18@gmail.com>
2020-05-07 20:27:22 +0700

    Updates

---
+++ precache-manifest.69d269acfce5cd876defc98fd32676dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57a1c2e980e092d48d3b0fd5831aa1ae",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "d1839e616cde5a6b9c5c",
+    "url": "/pokedex/static/js/2.6192b9cd.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/pokedex/static/js/2.6192b9cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8ee7ce313f559961175",
+    "url": "/pokedex/static/js/main.eb578e42.chunk.js"
+  },
+  {
+    "revision": "ed5a9742913badf7a85c",
+    "url": "/pokedex/static/js/runtime-main.82622ded.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pokedex-gh-pages/precache-manifest.9086ccc5e7f66b8013b9271933a2b951.js"
+  "/pokedex/precache-manifest.69d269acfce5cd876defc98fd32676dd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pokedex-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pokedex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
